649ba6aa07bb5f58ca9574f492bbc999cdaa4f9e
tessalu239 <minhthulu2399@gmail.com>
2023-10-23 19:10:24 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const display1= document.querySelector('.display-1');
+const display2= document.querySelector('.display-2');
+const displayResult= document.querySelector('.display-result');
+const displayNum= document.querySelectorAll('.number');
+const displayOp= document.querySelectorAll('.operators');
+const displayClear= document.querySelector('.clear');
+const displayClearEntity= document.querySelector('.clear-entity');
+const displayDot=document.querySelector('.dot');
+const display0=document.querySelector('.btn-0');
+const displayEqual=document.querySelector('.btn-equal');
+
+let dis2='';
+let dis1='';
+let disResult=null;
+let lastOPeration;//will need to understand it
+
